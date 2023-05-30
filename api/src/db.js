a6415aae655adcabf54c2c6c3787db7fdb5076f8
EnzoMagurno <magurnoenzo31602@gmail.com>
2023-05-30 20:20:43 -0300

    DB_DEPLOY

--- api/src/db.js
+++ api/src/db.js
@@ -2,16 +2,21 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-// const Activity = require('./models/Activity');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-  {
-    logging: false,
-    native: false,
-  }
-);
+const { DB_DEPLOY } = process.env;
+// const Activity = require('./models/Activity');
+const sequelize = new Sequelize(DB_DEPLOY, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false,
+});
+
+// const sequelize = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+//   {
+//     logging: false,
+//     native: false,
+//   }
+// );
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
